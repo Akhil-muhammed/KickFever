eb0d5d8a41fe8f63f89ed684cd01bec35d99db51
Akhil-muhammed <140682760+Akhil-muhammed@users.noreply.github.com>
2023-08-01 16:21:42 +0530

    football

---
+++ leag.js
@@ -0,0 +1,30 @@
+function real(){
+    document.getElementById('clubimage').style.display="block";
+    document.getElementById('trophy').src="https://wallpaperaccess.com/full/1923355.jpg";
+
+}
+function barca(){
+    document.getElementById('trophy').src="https://e1.pxfuel.com/desktop-wallpaper/0/588/desktop-wallpaper-fc-barcelona-on-twitter-barcelona-kit-2023.jpg";
+
+}
+function bayern(){
+    document.getElementById('trophy').src="https://w0.peakpx.com/wallpaper/371/43/HD-wallpaper-fc-bayern-bayern-munich-players.jpg";
+
+}
+function arsenal(){
+    document.getElementById('trophy').src="https://play-lh.googleusercontent.com/imPy8IDfghfXwvJwYL7zwce513DaNhabYLJwfeJStFDahwE865kbdIYOlBqKsY9uoDA";
+}
+
+function chelsea(){
+    document.getElementById('trophy').src="https://cdn.apk-cloud.com/detail/screenshot/k0Yot55jau_S4lGatUAA6RuTORfGOBx3ljB00Dbtyvz9GVJLM6nDAi8i31_SMTIS8Q=h900.png";
+
+}
+function acmilan(){
+    document.getElementById('trophy').src="https://play-lh.googleusercontent.com/wHVbJgsDSJN8BuuslHj7RfMw_ZrMlhhEVcny_zeRnvouIK08QYylbRIvy1ecbbJcWIsB";
+
+}
+function machesterunited(){
+    document.getElementById('trophy').src="https://w0.peakpx.com/wallpaper/1017/646/HD-wallpaper-manchester-united-futbol.jpg";
+
+}
+
